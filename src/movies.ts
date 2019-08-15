34f14a2374412a433d974efc9f8767dee8aeb2a9
Jr Reyes <jr.evilboss@gmail.com>
2019-08-15 12:44:55 +0800

    remove log

--- src/movies.ts
+++ src/movies.ts
@@ -3,7 +3,6 @@ import Movie from "./movie";
 export default class Movies {
     private _Movies = [];
     public getMovies = (): Array<any> => {
-        console.log(this._Movies);
         return this._Movies;
     };
     public addMovies = (Movie): void => {
